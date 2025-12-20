dd944257211003c2aa3f435e7c02433b250ee164
Noitiro <157133387+Noitiro@users.noreply.github.com>
2025-12-20 15:43:53 +0100

    add

--- Assets/Scripts/workshop/AddingIngredients.cs
+++ Assets/Scripts/workshop/AddingIngredients.cs
@@ -1,12 +1,31 @@
 using UnityEngine;
 public class AddingIngredients : MonoBehaviour {
     [SerializeField] SelectedIngredients selectedIngredients;
+    [SerializeField] GameObject prefabSalt;
+    [SerializeField] GameObject prefabMercury;
+    [SerializeField] GameObject prefabSulphur;
+
 
     [SerializeField] GameObject itemSlot;
     [SerializeField] GameObject itemSlot1;
     [SerializeField] GameObject itemSlot3;
 
-    private void Update () {
-
+    public void putIngredients () {
+        switch (selectedIngredients.selectIng) {
+            case AlchemyEnums.Ingredients.Salt:
+                Instantiate(prefabSalt, itemSlot.transform);
+                Instantiate(prefabSalt, itemSlot1.transform);
+                Instantiate(prefabSalt, itemSlot3.transform);
+                Debug.Log("Put salt");
+                break;
+            case AlchemyEnums.Ingredients.Mercury:
+                Instantiate(prefabMercury, itemSlot.transform);
+                Debug.Log("Put mercury");
+                break;
+            case AlchemyEnums.Ingredients.Sulphur:
+                Instantiate(prefabSulphur, itemSlot.transform);
+                Debug.Log("Put sulphur");
+                break;
+        }
     }
 }
