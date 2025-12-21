e1d9b8f054d0f00ca03ff60517487180a08da974
Noitiro <157133387+Noitiro@users.noreply.github.com>
2025-12-21 11:17:38 +0100

    add prefab finalitem

--- Assets/Scripts/workshop/ListIngedients.cs
+++ Assets/Scripts/workshop/ListIngedients.cs
@@ -5,8 +5,11 @@ public class ListIngedients : MonoBehaviour {
     [SerializeField] private List<GameObject> allIngridientsPrefab;
 
     public GameObject test(AlchemyEnums.Ingredients? nameIngredients) {
+
         switch (nameIngredients) {
             case AlchemyEnums.Ingredients.SublimatedSulphur:
+                Debug.Log(allIngridientsPrefab[0].name);
+                Debug.Log(nameIngredients.Value.ToString() + " xddd");
                 return allIngridientsPrefab[0];
             default:
                 return allIngridientsPrefab[1];
